3561a2c9f07c3c476851e54550dcf5cce4cffd53
Michael Han <hanmichael@google.com>
2025-05-25 23:55:21 -0700

    Remove dotenv

--- garbage_collection/config.py
+++ garbage_collection/config.py
@@ -1,8 +1,4 @@
 import os
-from dotenv import load_dotenv # type: ignore
-
-# Load environment variables from .env
-load_dotenv()
 
 # Default mount path for Filestore
 FILERESTORE_MOUNT_PATH = os.getenv("FILERESTORE_MOUNT_PATH", "/mnt/filestore")
